8f217c3cfd2fb4e24e9ab9c44985151eec17ba17
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-11-28 11:39:35 -0800

    10 min commit

--- main.cpp
+++ main.cpp
@@ -9,7 +9,9 @@ int mainMenu();
 int main() {
     
 
+    int userPick;
 
+    userPick = mainMenu();
 
     return 0;
 }
@@ -19,9 +21,18 @@ int main() {
 int mainMenu() {
 
     int userOption;
+    // menu string to display to user
+    string menuDisplay = "** Binary Search Tree ** - Select an option below:\n[1] Add Record\n[2] Delete\n[3] Search\n[4] Modify\n[5] Quit";
 
-    string menuDisplay = "Binary Search Tree - Select an option below:"
+    cout << menuDisplay;
+    cin >> userOption;
 
+    // input validation - make sure user inputs bewteen 1 and 4 or keep asking
+    while (userOption < 1 || userOption > 5) {
+        cout << "Please select valid option between 1 and 5" << endl;
+        cout << menuDisplay;
+        cin >> userOption;
+    }
 
-
+    return userOption;
 }
